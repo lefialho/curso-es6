72b508162d1134f0e4a33c38150dca2eb05f978e
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-08-14 12:28:56 -0300

    Animação ao scroll com class refatorado

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,25 +1,26 @@
-import {
-  active
-} from './config.js';
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindow = window.innerHeight * 0.6;
+    this.activeClass = 'active';
 
-export default function initScrollAnimation() {
-  const sections = document.querySelectorAll('[data-anime^="scroll"]');
-  const halfWindow = window.innerHeight * 0.6;
+    this.scrollAnimation = this.scrollAnimation.bind(this);
+  }
 
-  function scrollAnimation() {
-    sections.forEach((section) => {
+  scrollAnimation() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - halfWindow) < 0;
+      const isSectionVisible = (sectionTop - this.halfWindow) < 0;
 
       if (isSectionVisible)
-        section.classList.add(active);
-      else if (section.classList.contains(active))
-        section.classList.remove(active);
+        section.classList.add(this.activeClass);
+      else if (section.classList.contains(this.activeClass))
+        section.classList.remove(this.activeClass);
     });
   }
 
-  if (sections.length) {
-    scrollAnimation();
-    window.addEventListener('scroll', scrollAnimation);
+  init() {
+    this.scrollAnimation();
+    window.addEventListener('scroll', this.scrollAnimation);
   }
 }
