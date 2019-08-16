5910dcba483124be6af2b11d9ec17813258f80ff
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-08-16 12:42:41 -0300

    config deletado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -4,20 +4,8 @@ export default function fetchBitcoin(url, target) {
   .then(bitcoin => {
     const btcPrice = document.querySelector(target);
     btcPrice.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-    // console.log(bitcoin.BRL.sell)
   }).catch(error => {
     error = 'Problemas na configuração do fetch';
     console.log(Error(error));
   })
 }
-
-
-
-// async function fetchAnimals(url) {
-//   const animalsResponse = await fetch(url);
-//   const animalsJson = await animalsResponse.json();
-
-//  console.log(animalsJson.BRL.sell);
-// }
-
-// fetchAnimals('https://blockchain.info/ticker')
